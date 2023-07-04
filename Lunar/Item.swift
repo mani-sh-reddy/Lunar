c1a6c1ebeb05b4ef9506e9db2ea176326b74e69f
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-07-04 12:32:24 +0100

    initial boilerplate code

---
+++ Lunar/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Lunar
+//
+//  Created by Mani on 04/07/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Lunar/LunarApp.swift
@@ -0,0 +1,20 @@
+//
+//  LunarApp.swift
+//  Lunar
+//
+//  Created by Mani on 04/07/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LunarApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
