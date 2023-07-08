c24f5f2ceeb1858dfbc9f5e885750763f20949ab
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-07-08 19:57:16 +0100

    updated postslistview to use the new fetcher class

--- Lunar/HomeView.swift
+++ Lunar/HomeView.swift
@@ -10,26 +10,32 @@ import Kingfisher
 import SwiftUI
 
 struct HomeView: View {
-    @StateObject private var fetcher = Fetcher<CommunityModel>()
+    @StateObject private var trendingCommunitiesFetcher = Fetcher<CommunityModel>()
     @State var communityModel = CommunityModel(communities: [])
 
     var body: some View {
         NavigationView {
             List {
                 Section(header: Text("Feed")) {
-                    NavigationLink {
-                        PlaceholderView()
+                    NavigationLink { PostsListView(feedType: "Local", feedSort: "Active")
                     } label: {
                         FeedTypeRowView(feedType: "Local", icon: "house.circle.fill", iconColor: Color.green)
                     }
-                    NavigationLink {
-                        PostsListView(feedType: "All")
+                    NavigationLink { PostsListView(feedType: "All", feedSort: "Active")
                     } label: {
                         FeedTypeRowView(feedType: "All", icon: "building.2.crop.circle.fill", iconColor: Color.cyan)
                     }
+                    NavigationLink { PostsListView(feedType: "All", feedSort: "TopWeek")
+                    } label: {
+                        FeedTypeRowView(feedType: "Top", icon: "chart.line.uptrend.xyaxis.circle.fill", iconColor: Color.pink)
+                    }
+                    NavigationLink { PostsListView(feedType: "All", feedSort: "New")
+                    } label: {
+                        FeedTypeRowView(feedType: "New", icon: "star.circle.fill", iconColor: Color.yellow)
+                    }
                 }
                 Section(header: Text("Trending")) {
-                    ForEach(fetcher.result?.communities ?? [], id: \.community.id) { communities in
+                    ForEach(trendingCommunitiesFetcher.result?.communities ?? [], id: \.community.id) { communities in
                         NavigationLink {
                             CommunityInfoView(community: communities.community)
                         } label: {
@@ -58,7 +64,7 @@ struct HomeView: View {
         prefetcher.start()
 
         let urlString = "https://lemmy.world/api/v3/community/list?sort=Hot&limit=5"
-        fetcher.fetchResponse(urlString: urlString)
+        trendingCommunitiesFetcher.fetchResponse(urlString: urlString)
     }
 }
 
--- Lunar/PostsListView.swift
+++ Lunar/PostsListView.swift
@@ -9,12 +9,14 @@ import Kingfisher
 import SwiftUI
 
 struct PostsListView: View {
-    @ObservedObject var allPosts = AllPostsListLoader()
+    @StateObject private var fetcher = Fetcher<PostsModel>()
+    @State var postsModel = PostsModel(posts: [])
     var feedType: String
+    var feedSort: String
 
     var body: some View {
         List {
-            ForEach(allPosts.posts, id: \.post.id) { post in
+            ForEach(fetcher.result?.posts ?? [], id: \.post.id) { post in
                 Section {
                     NavigationLink {
                         Link(String(post.post.url ?? "Link"), destination: URL(string: post.post.url ?? "") ?? URL(string: "lemmy.world")!)
@@ -25,36 +27,32 @@ struct PostsListView: View {
                         }
                     }
                 }
-                .onAppear {
-                    let thumbnailURLs = allPosts.thumbnailURLsStrings.compactMap { URL(string: $0) }
-                    let prefetcher = ImagePrefetcher(urls: thumbnailURLs) {
-                        _, _, _ in
-                    }
-                    prefetcher.start()
-                }
             }
         }
+        .onAppear {
+            fetchPostsList(feedType: feedType, feedSort: feedSort)
+        }
+
         .navigationTitle(feedType)
         .listStyle(.grouped)
+    }
+
+    func fetchPostsList(feedType: String, feedSort: String) {
+        let thumbnailURLs = postsModel.thumbnailURLs.compactMap { URL(string: $0) }
+        let prefetcher = ImagePrefetcher(urls: thumbnailURLs) {
+            _, _, _ in
+        }
+        prefetcher.start()
+
+        let urlString = "https://lemmy.world/api/v3/post/list?type_=\(feedType)&sort=\(feedSort)&limit=50"
+        fetcher.fetchResponse(urlString: urlString)
     }
 }
 
 struct PostsListView_Previews: PreviewProvider {
     static var previews: some View {
-        let loader = AllPostsListLoader()
-        loader.posts = [
-            PostElement(
-                post: PostObject(id: 1, name: "Example Post", url: "https://example.com", creatorID: 1, communityID: 1, removed: false, locked: false, published: "2023-07-06T12:34:56Z", deleted: false, thumbnailURL: "https://example.com/thumbnail.jpg", apID: "exampleID", local: true, languageID: 1, featuredCommunity: false, featuredLocal: false, body: "This is an example post", updated: nil, embedTitle: nil, embedDescription: nil, embedVideoURL: nil),
-                creator: Creator(id: 1, name: "JohnDoe", displayName: "John Doe", avatar: nil, banned: false, published: "2023-07-06T12:34:56Z", actorID: "johnDoeActorID", bio: "I'm a user", local: true, banner: nil, deleted: false, admin: false, botAccount: false, instanceID: 1, updated: nil, matrixUserID: nil),
-                community: Community(id: 1, name: "ExampleCommunity", title: "Example Community", description: "This is an example community", removed: false, published: "2023-07-06T12:34:56Z", updated: nil, deleted: false, actorID: "exampleCommunityActorID", local: true, icon: nil, hidden: false, postingRestrictedToMods: false, instanceID: 1, banner: nil),
-                creatorBannedFromCommunity: false,
-                counts: Counts(id: 1, postID: 1, comments: 5, score: 10, upvotes: 15, downvotes: 5, published: "2023-07-06T12:34:56Z", newestCommentTimeNecro: "2023-07-06T12:34:56Z", newestCommentTime: "2023-07-06T12:34:56Z", featuredCommunity: false, featuredLocal: false, hotRank: 1, hotRankActive: 1), subscribed: .notSubscribed, saved: false, read: false, creatorBlocked: false, unreadComments: 3
-            )
-        ]
+        let mockPost = MockPost.mockPost
 
-        let onePostLoader = AllPostsListLoader()
-        onePostLoader.posts = Array(loader.posts.prefix(1))
-
-        return PostsListView(allPosts: onePostLoader, feedType: "All")
+        PostsListView(postsModel: mockPost, feedType: "All", feedSort: "Active")
     }
 }
