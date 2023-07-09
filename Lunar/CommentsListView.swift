2af315e97f9bccc4f413c0f0ec4e5845c35b6272
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-07-09 22:04:04 +0100

    testing comments view hierarchy

--- Lunar/CommentsListView.swift
+++ Lunar/CommentsListView.swift
@@ -13,19 +13,41 @@ struct CommentsListView: View {
     @StateObject private var fetcher = Fetcher<CommentsListModel>()
     @State var commentsListModel = CommentsListModel(comments: [])
     @State var postId: Int
+    @State var commentMaxDepth: Int = 1
 
     var body: some View {
         ScrollView {
-            ForEach(fetcher.result?.comments ?? [], id: \.comment.id) { comment in
-                VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Text(String(commentMaxDepth)).font(.largeTitle)
+                Button("increase depth") {
+                    commentMaxDepth += 1
+                    loadComments()
+                }.font(.largeTitle)
+                    .background(.gray)
+                    .foregroundStyle(.white)
+                    .buttonBorderShape(.capsule)
+                Button("decrease depth") {
+                    commentMaxDepth -= 1
+                    loadComments()
+                }.font(.largeTitle)
+                    .background(.gray)
+                    .foregroundStyle(.white)
+                    .buttonBorderShape(.capsule)
+                ForEach(fetcher.result?.comments ?? [], id: \.comment.id) { comment in
+
+                    Text(comment.comment.path)
                     Text(comment.comment.content)
-                    Text("")
+                    Text(comment.creator.name).bold()
                 }
             }
             .onAppear {
-                let urlString = "https://lemmy.world/api/v3/comment/list?type_=All&sort=Top&limit=10&post_id=\(postId)"
-                fetcher.fetchResponse(urlString: urlString)
+                loadComments()
             }
         }.padding()
+    }
+    
+    func loadComments(){
+        let urlString = "https://lemmy.world/api/v3/comment/list?type_=All&sort=Top&limit=50&post_id=\(postId)&max_depth=\(commentMaxDepth)"
+        fetcher.fetchResponse(urlString: urlString)
     }
 }
