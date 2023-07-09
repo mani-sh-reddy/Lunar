9ee45fc8048282bfcb135082599cd4f2f33fd107
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-07-09 20:34:46 +0100

    removed navigation view from PostListView

--- Lunar/PostsListView.swift
+++ Lunar/PostsListView.swift
@@ -16,20 +16,26 @@ struct PostsListView: View {
     var communityId: Int = 0
 
     var body: some View {
-        List {
+        ScrollView {
             ForEach(postsListFetcher.posts, id: \.post.id) { post in
-                Section {
-                    NavigationLink {
-                        Link(String(post.post.url ?? "Link"), destination: URL(string: post.post.url ?? "") ?? URL(string: "lemmy.world")!)
-                        CommentsListView(postId: post.post.id)
-                    } label: {
-                        VStack {
-                            PostRowView(post: post)
-                        }
-                    }
-                }
+//                    NavigationLink {
+//                        Link(String(post.post.url ?? "Link"), destination: URL(string: post.post.url ?? "") ?? URL(string: "lemmy.world")!)
+//                        CommentsListView(postId: post.post.id)
+//                    } label: {
+
+                PostRowView(post: post)
+                Rectangle()
+                    .fill(Color.gray).opacity(0.2)
+                    .frame(height: 30)
+                    .edgesIgnoringSafeArea(.horizontal)
             }
         }
+        .overlay(Group {
+            if !postsListFetcher.isLoaded {
+                ProgressView("Fetching")
+                    .frame(width: 100)
+            }
+        })
         .task {
             let baseURL = "https://lemmy.world/api/v3/post/list?sort=\(feedSort)&limit=50&"
             let nonSpecificUrlPrefix = "type_=\(feedType)"
@@ -39,11 +45,6 @@ struct PostsListView: View {
             postsListFetcher.fetch(endpoint: endpoint)
         }
         .navigationTitle(viewTitle)
-        .listStyle(.grouped)
-        .overlay(Group {
-            if !postsListFetcher.isLoaded {
-                ProgressView("Fetching")
-            }
-        })
+//        .listStyle(.grouped)
     }
 }
