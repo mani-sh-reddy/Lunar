09b5d7748da0c38f95761966194f00caead39547
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-07-27 00:26:45 +0100

    renamed file

--- Lunar/Settings Views/SettingsSectionView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SettingsSectionView.swift
-//  Lunar
-//
-//  Created by Mani on 23/07/2023.
-//
-
-import Foundation
---
+++ Lunar/Settings.swift
@@ -0,0 +1,15 @@
+//
+//  Settings.swift
+//  Lunar
+//
+//  Created by Mani on 23/07/2023.
+//
+
+import Foundation
+
+enum Settings {
+    static let instanceHostURL: String = "lemmy.world"
+    static let displayName: String = "Mani"
+    static let userName: String = "mani"
+    static let debugModeEnabled: Bool = false
+}
