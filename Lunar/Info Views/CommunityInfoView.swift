19287077abdd29ea860ab6662d0d0f13317b8ea8
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-08-06 12:39:40 +0100

    added progress view on info views

--- Lunar/Info Views/CommunityInfoView.swift
+++ Lunar/Info Views/CommunityInfoView.swift
@@ -9,6 +9,14 @@ import Kingfisher
 import SwiftUI
 
 struct CommunityInfoView: View {
+    @AppStorage("debugModeEnabled") var debugModeEnabled = Settings.debugModeEnabled
+
+    @State private var bannerFailedToLoad = false
+    @State private var avatarFailedToLoad = false
+
+    @State private var loadingOpacity: Double = 1
+    @State private var progressViewValue: Double = 0
+
     var community: SearchCommunityInfo
     var hasBanner: Bool {
         community.banner != "" && community.banner != nil
@@ -19,44 +27,88 @@ struct CommunityInfoView: View {
     }
 
     var body: some View {
-        ScrollView {
-            if hasBanner {
-                ZStack {
+        ZStack {
+            ScrollView {
+                if hasBanner, !bannerFailedToLoad {
                     KFImage(URL(string: community.banner ?? ""))
                         .resizable()
+                        .fade(duration: 0.25)
+                        .onFailure { _ in
+                            withAnimation(.bouncy) {
+                                bannerFailedToLoad = true
+                            }
+                        }
                         .scaledToFit()
                         .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                        .padding()
+                        .padding(5)
+                        .padding(.horizontal, 10)
+                        .border(debugModeEnabled ? Color.red : Color.clear)
                 }
-                .padding(.bottom, 50)
+
+                VStack(alignment: .leading) {
+                    HStack {
+                        if hasAvatar, !avatarFailedToLoad {
+                            KFImage(URL(string: community.icon ?? ""))
+                                .fade(duration: 0.25)
+                                .resizable()
+                                .onFailure { _ in
+                                    withAnimation(.snappy) {
+                                        avatarFailedToLoad = true
+                                    }
+                                }
+                                .clipped()
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(Circle())
+                                .frame(width: 75, height: 75)
+                                .padding(5)
+                                .border(debugModeEnabled ? Color.red : Color.clear)
+                        } else {
+                            EmptyView()
+                                .frame(width: 0, height: 0)
+                        }
+                        VStack(alignment: .leading) {
+                            Text(community.name)
+                                .font(.title).bold()
+                            Text("@\(URLParser.extractDomain(from: community.actorID))").font(.headline)
+                        }
+                    }.padding(.bottom, 10)
+
+                    if let description = community.description {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .foregroundStyle(.ultraThinMaterial)
+                            Text(description)
+                                .padding(10)
+                        }
+                    }
+                }.padding()
             }
-
-            VStack(alignment: .leading) {
-                HStack {
-                    if hasAvatar {
-                        KFImage(URL(string: community.icon ?? ""))
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                            .frame(maxWidth: 50, maxHeight: 50)
-                            .padding(5)
-                    }
-                    VStack(alignment: .leading) {
-                        Text(community.name)
-                            .font(.title).bold()
-                        Text("@\(URLParser.extractDomain(from: community.actorID))").font(.headline)
-                    }
-                }.padding(.bottom, 10)
-
-                if let description = community.description {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .foregroundStyle(.ultraThinMaterial)
-                        Text(description)
-                            .padding(10)
-                    }
-                }
-            }.padding()
+            Rectangle()
+                .foregroundStyle(.background)
+                .opacity(loadingOpacity)
+            ProgressView(value: progressViewValue)
+                .frame(width: 100)
+                .opacity(loadingOpacity)
         }
+        /// Workaround to stop blank space appearing and disappearing
+        /// when loading an invalid image
+        .task {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
+                withAnimation(.smooth) {
+                    progressViewValue += 1
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation {
+                    loadingOpacity = 0
+                }
+            }
+        }
+    }
+}
+
+struct CommunityInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunityInfoView(community: MockData.communityInfoView4)
     }
 }
--- Lunar/Info Views/UserInfoView.swift
+++ Lunar/Info Views/UserInfoView.swift
@@ -9,7 +9,16 @@ import Kingfisher
 import SwiftUI
 
 struct UserInfoView: View {
+    @AppStorage("debugModeEnabled") var debugModeEnabled = Settings.debugModeEnabled
+
+    @State private var loadingOpacity: Double = 1
+    @State private var progressViewValue: Double = 0
+
+    @State private var bannerFailedToLoad = false
+    @State private var avatarFailedToLoad = false
+
     var person: Creator
+
     var hasBanner: Bool {
         person.banner != "" && person.banner != nil
     }
@@ -19,49 +28,140 @@ struct UserInfoView: View {
     }
 
     var body: some View {
-        ScrollView {
-            if hasBanner {
-                ZStack {
+        ZStack {
+            ScrollView {
+                if hasBanner, !bannerFailedToLoad {
                     KFImage(URL(string: person.banner ?? ""))
                         .resizable()
+                        .fade(duration: 0.25)
+                        .onFailure { _ in
+                            withAnimation(.bouncy) {
+                                bannerFailedToLoad = true
+                            }
+                        }
                         .scaledToFit()
                         .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                        .padding()
+                        .padding(5)
+                        .padding(.horizontal, 10)
+                        .border(debugModeEnabled ? Color.red : Color.clear)
+                }
+
+                VStack(alignment: .leading) {
+                    HStack {
+                        if hasAvatar, !avatarFailedToLoad {
+                            KFImage(URL(string: person.avatar ?? ""))
+                                .fade(duration: 0.25)
+                                .resizable()
+                                .onFailure { _ in
+                                    withAnimation(.snappy) {
+                                        avatarFailedToLoad = true
+                                    }
+                                }
+                                .clipped()
+                                .aspectRatio(contentMode: .fill)
+                                .clipShape(Circle())
+                                .frame(width: 75, height: 75)
+                                .padding(5)
+                                .border(debugModeEnabled ? Color.red : Color.clear)
+                        } else {
+                            EmptyView()
+                                .frame(width: 0, height: 0)
+                        }
+                        VStack(alignment: .leading) {
+                            Text(person.name)
+                                .font(.title).bold()
+                            Text("@\(URLParser.extractDomain(from: person.actorID))").font(.headline)
+                        }
+                    }.padding(.bottom, 10)
+
+                    if let description = person.bio {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .foregroundStyle(.ultraThinMaterial)
+                            Text(description)
+                                .padding(10)
+                        }
+                    }
+                }.padding()
+            }
+            Rectangle()
+                .foregroundStyle(.background)
+                .opacity(loadingOpacity)
+            ProgressView(value: progressViewValue)
+                .frame(width: 100)
+                .opacity(loadingOpacity)
+        }
+        /// Workaround to stop blank space appearing and disappearing
+        /// when loading an invalid image
+        .task {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
+                withAnimation(.smooth) {
+                    progressViewValue += 1
                 }
             }
-
-            VStack(alignment: .leading) {
-                HStack {
-                    if hasAvatar {
-                        KFImage(URL(string: person.avatar ?? ""))
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-                            .frame(maxWidth: 50, maxHeight: 50)
-                            .padding(5)
-                    }
-                    VStack(alignment: .leading) {
-                        Text(person.name)
-                            .font(.title).bold()
-                        Text("@\(URLParser.extractDomain(from: person.actorID))").font(.headline)
-                    }
-                }.padding(.bottom, 10)
-
-                if let bio = person.bio {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .foregroundStyle(.ultraThinMaterial)
-                        Text(bio)
-                            .padding(10)
-                    }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation {
+                    loadingOpacity = 0
                 }
-            }.padding()
+            }
         }
     }
 }
 
+// struct UserInfoView: View {
+//
+//    var hasBanner: Bool {
+//        person.banner != "" && person.banner != nil
+//    }
+//
+//    var hasAvatar: Bool {
+//        person.avatar != "" && person.avatar != nil
+//    }
+//
+//    var body: some View {
+//        ScrollView {
+//            if hasBanner {
+//                ZStack {
+//                    KFImage(URL(string: person.banner ?? ""))
+//                        .resizable()
+//                        .scaledToFit()
+//                        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+//                        .padding()
+//                }
+//            }
+//
+//            VStack(alignment: .leading) {
+//                HStack {
+//                    if hasAvatar {
+//                        KFImage(URL(string: person.avatar ?? ""))
+//                            .resizable()
+//                            .scaledToFit()
+//                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+//                            .frame(maxWidth: 50, maxHeight: 50)
+//                            .padding(5)
+//                    }
+//                    VStack(alignment: .leading) {
+//                        Text(person.name)
+//                            .font(.title).bold()
+//                        Text("@\(URLParser.extractDomain(from: person.actorID))").font(.headline)
+//                    }
+//                }.padding(.bottom, 10)
+//
+//                if let bio = person.bio {
+//                    ZStack {
+//                        RoundedRectangle(cornerRadius: 10, style: .continuous)
+//                            .foregroundStyle(.ultraThinMaterial)
+//                        Text(bio)
+//                            .padding(10)
+//                    }
+//                }
+//            }.padding()
+//        }
+//    }
+// }
+
 struct UserInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        UserInfoView(person: MockData.userInfoView)
+        UserInfoView(person: MockData.userInfoView2)
     }
 }
