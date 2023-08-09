0b360da63a4529c0b2c794301c5e1e7e82d52fe2
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-08-09 23:52:31 +0100

    lint updates

--- Lunar/Login Views/AddNewUserButtonView.swift
+++ Lunar/Login Views/AddNewUserButtonView.swift
@@ -9,24 +9,21 @@ import SwiftUI
 
 struct AddNewUserButtonView: View {
     @Binding var showingPopover: Bool
-
     var body: some View {
-        Button(action: {
-            withAnimation(.linear(duration: 1)) {
-                showingPopover = true
-                // need to invalidate all inputs on first popover
-            }
-        }
-        ) {
+        Button(action: addNewUserButtonAction) {
             Label {
-                Text("Add User")
-                    .foregroundStyle(.blue)
+                Text("Add User").foregroundStyle(.blue)
             } icon: {
                 Image(systemName: "plus.circle.fill")
                     .font(.title2)
                     .foregroundStyle(.blue)
                     .symbolRenderingMode(.hierarchical)
             }
+        }
+    }
+    private func addNewUserButtonAction() {
+        withAnimation(.linear(duration: 1)) {
+            showingPopover = true
         }
     }
 }
