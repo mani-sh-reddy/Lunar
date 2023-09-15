0a6134e48a761e6d7eb4548baf4edce995a625b3
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-09-15 23:55:04 +0100

    #patch hold comments list view with new view

--- Lunar/Comment View/CommentSectionView.swift
+++ Lunar/Comment View/CommentSectionView.swift
@@ -32,15 +32,11 @@ struct CommentSectionView: View {
     let nestedComments = comments.nestedComment
 
     List {
-      ForEach(nestedComments, id: \.id) { comment in
-        RecursiveComment(nestedComment: comment)
-      }
-
       Section {
         PostRowView(
           upvoted: $upvoted, downvoted: $downvoted, isSubscribed: communityIsSubscribed, post: post, insideCommentsView: true
         )
-//        .environmentObject(postsFetcher)
+        //        .environmentObject(postsFetcher)
         InPostActionsView(post: post)
         if !postBody.isEmpty {
           VStack(alignment: .trailing) {
@@ -48,60 +44,19 @@ struct CommentSectionView: View {
           }
         }
       }
-      .listRowSeparator(.hidden)
-      .listRowBackground(Color.clear)
       Section {
-        ForEach(comments.indices, id: \.self) { index in
-          let comment = comments[index]
-          if !comment.isCollapsed, !comment.isShrunk {
-            CommentRowView(
-              collapseToIndex: $collapseToIndex,
-              collapserPath: $collapserPath,
-              comment: comment,
-              listIndex: index,
-              comments: comments
-            ).id(UUID())
-              .environmentObject(commentsFetcher)
-          } else if !comment.isCollapsed, comment.isShrunk {
-            HStack {
-              Text("Collapsed").italic().foregroundStyle(.secondary).font(.caption)
-              Spacer()
-            }
-            .contentShape(Rectangle())
-            .onTapGesture {
-              commentExpandAction(comment: comment)
-            }
-            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-              Button {
-                commentExpandAction(comment: comment)
-              } label: {
-                Label("expand", systemImage: "arrow.up.left.and.arrow.down.right.circle.fill")
-              }
-              .tint(.blue)
-            }
-          }
+        ForEach(nestedComments, id: \.id) { comment in
+          RecursiveComment(nestedComment: comment).id(UUID())
         }
       }
     }
     .listStyle(.grouped)
   }
-
-  func commentExpandAction(comment: CommentObject) {
-    for commentOnMainList in comments {
-      if commentOnMainList.comment.path.contains(comment.comment.path) {
-        if commentOnMainList.comment.path != comment.comment.path {
-          commentsFetcher.updateCommentCollapseState(commentOnMainList, isCollapsed: false)
-        } else {
-          commentsFetcher.updateCommentShrinkState(commentOnMainList, isShrunk: false)
-        }
-      }
-    }
-  }
 }
 
 struct RecursiveComment: View {
   @State private var isExpanded = true
-  
+
   let commentHierarchyColors: [Color] = [
     .clear,
     .red,
@@ -113,16 +68,16 @@ struct RecursiveComment: View {
     .indigo,
     .purple,
   ]
-  
+
   let nestedComment: NestedComment
-  
+
   let haptics = UIImpactFeedbackGenerator(style: .soft)
-  
+
   var body: some View {
     if isExpanded {
       let indentLevel = min(nestedComment.indentLevel, commentHierarchyColors.count - 1)
       let color = commentHierarchyColors[indentLevel]
-      
+
       HStack {
         if nestedComment.indentLevel > 1 {
           Rectangle()
@@ -131,35 +86,41 @@ struct RecursiveComment: View {
             .padding(.vertical, 5)
         }
         Text(try! AttributedString(markdown: nestedComment.commentViewData.comment.content))
+        Spacer()
       }
+      .contentShape(Rectangle())
       .onTapGesture {
         isExpanded.toggle()
+        haptics.impactOccurred(intensity: 0.5)
       }
       .swipeActions(edge: .trailing, allowsFullSwipe: true) {
         swipeActions
       }
-      
+
       ForEach(nestedComment.subComments, id: \.id) { subComment in
-        RecursiveComment(nestedComment: subComment)
+        RecursiveComment(nestedComment: subComment).id(UUID())
           .padding(.leading, 10) // Add indentation
       }
     } else {
-      ZStack{
+      HStack {
         Text(try! AttributedString(markdown: "_Collapsed_"))
           .foregroundStyle(.gray)
           .font(.caption)
-        Rectangle().foregroundStyle(.clear).ignoresSafeArea()
-          .onTapGesture {
-            isExpanded.toggle()
-            haptics.impactOccurred(intensity: 0.5)
-          }
+        Spacer()
+      }
+      .contentShape(Rectangle())
+      .onTapGesture {
+        isExpanded.toggle()
+        haptics.impactOccurred(intensity: 0.5)
       }
     }
   }
+
   var swipeActions: some View {
     return Group {
       Button {
         isExpanded.toggle()
+        haptics.impactOccurred(intensity: 0.5)
       } label: {
         Label("collapse", systemImage: "arrow.up.to.line.circle.fill")
       }
@@ -171,41 +132,4 @@ struct RecursiveComment: View {
       }.tint(.orange)
     }
   }
-  
 }
-
-//    DisclosureGroup(
-//      isExpanded: $isExpanded,
-//      content: {
-//        ForEach(comment.subComments, id: \.id) { subComment in
-//          if isExpanded {
-//            RecursiveComment(comment: subComment)
-//          } else {
-//            EmptyView()
-//          }
-//        }
-//      },
-//      label: {
-//        if isExpanded {
-//          Text(try! AttributedString(markdown: comment.commentViewData.comment.content))
-//            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
-//              Button {
-//                isExpanded.toggle()
-//              } label: {
-//                Label("collapse", systemImage: "arrow.up.to.line.circle.fill")
-//              }
-//              .tint(.blue)
-//              Button {
-//                //        showCommentPopover = true
-//              } label: {
-//                Label("reply", systemImage: "arrowshape.turn.up.left.circle.fill")
-//              }.tint(.orange)
-//            }
-//        } else {
-//          Text(try! AttributedString(markdown: "_Collapsed_"))
-//            .foregroundStyle(.gray)
-//        }
-//      }
-//    )
-//  }
-//}
