0a155128e40e35bd97b33b41c3c48dea45f44b70
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-09-15 17:51:03 +0100

    #patch reorganised splash screen settings

--- Lunar/Settings Views/SettingsSplashScreenView.swift
+++ Lunar/Settings Views/SettingsSplashScreenView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 import WhatsNewKit
 
 struct SettingsSplashScreenView: View {
-  @AppStorage("showWelcomeScreen") var showWelcomeScreen = Settings.showWelcomeScreen
-//  @AppStorage("showLaunchSplashScreen") var showLaunchSplashScreen = Settings.showLaunchSplashScreen
+  @AppStorage("showLaunchSplashScreen") var showLaunchSplashScreen = Settings.showLaunchSplashScreen
   @AppStorage("clearWhatsNewDefaults") var clearWhatsNewDefaults = Settings.clearWhatsNewDefaults
   @AppStorage("clearInitialWhatsNewDefault") var clearInitialWhatsNewDefault = Settings.clearInitialWhatsNewDefault
   
@@ -20,22 +19,28 @@ struct SettingsSplashScreenView: View {
   
   var body: some View {
     List {
-      Button {
-        clearWhatsNewDefaults.toggle()
-        notificationHaptics.notificationOccurred(.success)
-        alertPresented = true
-      } label: {
-        Text("Clear All WhatsNewKit List")
+      Section {
+        Toggle(isOn: $showLaunchSplashScreen) {
+          Text("Launch Splash Screen")
+        }
+      } footer: {
+        Text("Show the Lunar logo splash screen for a couple of seconds every time the app is launched.")
       }
-      Button {
-        clearInitialWhatsNewDefault.toggle()
-        notificationHaptics.notificationOccurred(.success)
-        alertPresented = true
-      } label: {
-        Text("Clear WhatsNewKit Initial Launch")
-      }
-      Toggle(isOn: $showWelcomeScreen) {
-        Text("Show Welcome Screen")
+      Section {
+        Button {
+          clearWhatsNewDefaults.toggle()
+          notificationHaptics.notificationOccurred(.success)
+          alertPresented = true
+        } label: {
+          Text("Clear All WhatsNewKit List")
+        }
+        Button {
+          clearInitialWhatsNewDefault.toggle()
+          notificationHaptics.notificationOccurred(.success)
+          alertPresented = true
+        } label: {
+          Text("Clear WhatsNewKit Initial Launch")
+        }
       }
     }
     .alert(isPresented: $alertPresented) {
