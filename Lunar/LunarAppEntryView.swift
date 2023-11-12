a17a4456e7d3dd737f7ae3a7d0c1a7bd2a2508ce
Manish Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-11-12 22:15:48 +0000

    cleaned up code

--- Lunar/LunarAppEntryView.swift
+++ Lunar/LunarAppEntryView.swift
@@ -8,34 +8,60 @@
 import Defaults
 import Foundation
 import SwiftUI
+import WhatsNewKit
 
 @main
 struct LunarAppEntryView: App {
   @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
   @StateObject var dataCacheHolder = DataCacheHolder(appBundleID: DefaultValues.appBundleID)
+
   @Environment(\.scenePhase) var phase
+
+  @State var whatsNewFirstLaunch: WhatsNew? = WhatsNewKitData().initial
+//  @State var WhatsNew_2023_11: WhatsNew? = WhatsNewKitData().WhatsNew_2023_11
+
+  @Default(.accentColor) var accentColor
   @Default(.selectedTab) var selectedTab
+  @Default(.clearWhatsNewDefaults) var clearWhatsNewDefaults
+  @Default(.clearInitialWhatsNewDefault) var clearInitialWhatsNewDefault
+
+//  let whatsNewVersionStore = InMemoryWhatsNewVersionStore() /// ** Uncomment when testing **
+  let whatsNewVersionStore = UserDefaultsWhatsNewVersionStore()
 
   var body: some Scene {
     WindowGroup {
-      WhatsNewIntermediateView()
+      TabContentView()
+        .accentColor(accentColor)
+        .sheet(whatsNew: $whatsNewFirstLaunch, versionStore: whatsNewVersionStore)
+//        .sheet(whatsNew: $WhatsNew_2023_11, versionStore: whatsNewVersionStore)
         .environmentObject(dataCacheHolder)
-        .task {
-          AppearanceController.shared.setAppearance()
-        }
+        .task { AppearanceController.shared.setAppearance() }
     }
-    .onChange(of: phase) { newPhase in
-      switch newPhase {
-      case .active:
-        _ = "App is Active"
-      case .inactive:
-        _ = "App is Inactive"
-      case .background:
-        _ = "App in Background"
-        PhaseChangeActions().homeScreenQuickActions()
-      @unknown default:
-        _ = "Unknown App State"
-      }
+    .onChange(of: phase) { newPhase in phaseSwitch(phase: newPhase) }
+    .onChange(of: clearWhatsNewDefaults) { _ in whatsNewKitClearAll() }
+    .onChange(of: clearInitialWhatsNewDefault) { _ in whatsNewKitClearInitial() }
+  }
+
+  func whatsNewKitClearAll() {
+    whatsNewVersionStore.removeAll()
+  }
+
+  func whatsNewKitClearInitial() {
+    whatsNewVersionStore.remove(presentedVersion: "0.0.0")
+  }
+
+  func phaseSwitch(phase: ScenePhase) {
+    switch phase {
+    case .active:
+      _ = "App is Active"
+    case .inactive:
+      _ = "App is Inactive"
+    case .background:
+      _ = "App in Background"
+      PhaseChangeActions().homeScreenQuickActions()
+    @unknown default:
+      _ = "Unknown App State"
     }
   }
 }
--- Lunar/WhatsNewKitData/WhatsNewKitData.swift
+++ Lunar/WhatsNewKitData/WhatsNewKitData.swift
@@ -2,18 +2,9 @@
 //  WhatsNewKitData.swift
 //  Lunar
 //
-//  Created by Mani on 14/09/2023.
+//  Created by Mani on 12/11/2023.
 //
 
 import Foundation
-import SFSafeSymbols
-import SwiftUI
-import WhatsNewKit
 
 class WhatsNewKitData {}
-
-class WhatsNewKitCollection {
-  /// **Unreleased**
-//  var whatsNewArray: [WhatsNew] = [WhatsNewKitData().WhatsNew_2023_11]
-  var whatsNewArray: [WhatsNew] = []
-}
