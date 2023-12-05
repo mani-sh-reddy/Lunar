cbd1b298a3a2a141a8e5239482bd504da3496189
Mani Reddy <47571755+mani-sh-reddy@users.noreply.github.com>
2023-12-05 17:46:31 +0000

    added community app icon

--- Lunar/Settings Views/SettingsAppIconPickerView.swift
+++ Lunar/Settings Views/SettingsAppIconPickerView.swift
@@ -5,41 +5,53 @@
 //  Created by Mani on 27/07/2023.
 //
 
+import Collections
 import Defaults
 import Foundation
+import SFSafeSymbols
 import SwiftUI
 
 struct SettingsAppIconPickerView: View {
   @Default(.selectedAppIcon) var selectedAppIcon
 
-  private var appIcons = [
-    ("Default", "Default"),
-    ("Space", "Space"),
-    ("Lemming", "Lemming"),
-    ("Original", "v0"),
-    ("Night", "Night"),
-    ("Purple", "Purple"),
-    ("Kbin", "Kbin"),
-  ]
+  let notificationHaptics = UINotificationFeedbackGenerator()
+
+  private var appIcons: OrderedDictionary<String, [(String, String, String)]> {
+    return [
+      "Default": [
+        ("Default", "Default", "")
+      ],
+      "Community Icons": [
+        ("@MrSebSin", "MrSebSin", "https://lemmy.world/u/MrSebSin")
+      ],
+      "Created by Mani": [
+        ("Lemming", "Lemming", ""),
+        ("Kbin", "Kbin", ""),
+        ("Space", "Space", "")
+      ],
+      "Colors": [
+        ("Blue", "v0", ""),
+        ("Black", "Night", ""),
+        ("Purple", "Purple", "")
+      ]
+    ]
+  }
 
   var body: some View {
     List {
-      Picker("Icons", selection: $selectedAppIcon) {
-        ForEach(appIcons, id: \.1.self) { name, identifier in
-          Label {
-            Text(name)
-              .padding(.horizontal, 10)
-          } icon: {
-            Image("AppIconDownsized\(identifier)")
-              .resizable()
-              .frame(width: 50, height: 50)
-              .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+      ForEach(appIcons.elements, id: \.key) { heading, value in
+        Section {
+          Picker("Icons", selection: $selectedAppIcon) {
+            ForEach(value, id: \.1) { name, identifier, link in
+              AppIconRow(name: name, identifier: identifier, link: link)
+            }
           }
-          .tag("AppIcon\(identifier)")
-          .padding(.horizontal, 10)
+          .labelsHidden()
+          .pickerStyle(.inline)
+        } header: {
+          Text(heading)
         }
       }
-      .pickerStyle(.inline)
     }
     .onChange(of: selectedAppIcon) { newValue in
       if newValue == "AppIconDefault" {
@@ -47,8 +59,47 @@ struct SettingsAppIconPickerView: View {
       } else {
         UIApplication.shared.setAlternateIconName(newValue)
       }
+      notificationHaptics.notificationOccurred(.success)
     }
     .navigationTitle("App Icons")
+  }
+}
+
+struct AppIconRow: View {
+  var name: String
+  var identifier: String
+  var link: String
+
+  let defaultURL: String = "https://lemmy.world"
+
+  @State private var showSafari = false
+
+  var body: some View {
+    Label {
+      HStack(alignment: .center) {
+        if !link.isEmpty {
+          Button {
+            showSafari = true
+          } label: {
+            Text(name)
+          }
+        } else {
+          Text(name)
+        }
+      }
+      .padding(.horizontal, 10)
+    } icon: {
+      Image("AppIconScale\(identifier)")
+        .resizable()
+        .frame(width: 50, height: 50)
+        .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+    }
+    .tag("AppIcon\(identifier)")
+    .padding(.horizontal, 10)
+    .inAppSafari(
+      isPresented: $showSafari,
+      stringURL: link
+    )
   }
 }
 
